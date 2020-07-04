e46cb0964ded74b7fc46fea0850a48dbaf21f3b1
kanami <ray.knm1116@gmail.com>
2020-07-04 14:51:44 +0000

    PHP14　NEWSテーブルの作成

---
+++ database/migrations/2020_07_04_000641_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title'); //ニュースのタイトル
+            $table->string('body'); //ニュースの本文
+            $table->string('image_path')->nullable(); //画像のパス（必須ではない）
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
